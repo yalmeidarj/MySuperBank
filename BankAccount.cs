7e22ad904ed0ccc64da6ee4cb729a36e917c09e0
Yuri Almeida <73500564+yalmeidarj@users.noreply.github.com>
2022-07-17 14:19:29 -0400

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySuperBank
+{
+    public class BankAccount
+    {
+        public string Number { get; set; }
+        public string Owner { get; set; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+        private static int accountNumberSeed = 1234567890;
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balace");
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string Note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be postive");
+            }
+            var depoist = new Transaction(amount, date, Note);
+            allTransactions.Add(depoist);
+        }
+        public void MakeWithdrawal(decimal amount, DateTime date, string Note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of Withdrawal must be postive");
+            }
+            if (Balance - amount <= 0) 
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Not sufficient funs for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, Note);
+            allTransactions.Add(withdrawal);
+        }
+
+        public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+            //Header
+            report.AppendLine($"Date\t\tAmount\tNote");
+            foreach (var item in allTransactions)
+            {
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{item.Notes}");
+            }
+            return report.ToString();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Yuri", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}. ");
+
+            try
+            {
+                account.MakeDeposit(658, DateTime.Now, "payment received");
+                account.MakeWithdrawal(75, DateTime.Now, "Theatre");
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine($"Invalid Transaction");
+                Console.WriteLine(e.ToString());
+            }            
+            var myTransactions = account.GetAccountHistory();
+            Console.WriteLine(myTransactions);
+            Console.WriteLine(account.Balance);
+        }
+    }   
+}
